ace7fa7a0fe858acb5c4014643ef191bdfdc82a4
chuff <chad@thehuffs.net>
2025-03-22 07:28:30 -0600

    DE IA NE NH NJ TN MSPA USNAT (#65)
    * DE IA NE NH NJ TN
    
    * remove validation
    
    * usnat backwards compatibility
    
    ---------
    
    Co-authored-by: Chad Huff <chad@totalium.com>

---
+++ iabgpp-encoder/src/main/java/com/iab/gpp/encoder/segment/UsNhGpcSegment.java
@@ -0,0 +1,60 @@
+package com.iab.gpp.encoder.segment;
+
+import java.util.List;
+import com.iab.gpp.encoder.base64.AbstractBase64UrlEncoder;
+import com.iab.gpp.encoder.base64.CompressedBase64UrlEncoder;
+import com.iab.gpp.encoder.bitstring.BitStringEncoder;
+import com.iab.gpp.encoder.datatype.EncodableBoolean;
+import com.iab.gpp.encoder.datatype.EncodableFixedInteger;
+import com.iab.gpp.encoder.error.DecodingException;
+import com.iab.gpp.encoder.field.EncodableBitStringFields;
+import com.iab.gpp.encoder.field.UsNhField;
+
+public class UsNhGpcSegment extends AbstractLazilyEncodableSegment<EncodableBitStringFields> {
+
+  private AbstractBase64UrlEncoder base64UrlEncoder = CompressedBase64UrlEncoder.getInstance();
+  private BitStringEncoder bitStringEncoder = BitStringEncoder.getInstance();
+
+  public UsNhGpcSegment() {
+    super();
+  }
+
+  public UsNhGpcSegment(String encodedString) {
+    super();
+    this.decode(encodedString);
+  }
+
+  @Override
+  public List<String> getFieldNames() {
+    return UsNhField.USNH_GPC_SEGMENT_FIELD_NAMES;
+  }
+
+  @Override
+  protected EncodableBitStringFields initializeFields() {
+    EncodableBitStringFields fields = new EncodableBitStringFields();
+    fields.put(UsNhField.GPC_SEGMENT_TYPE, new EncodableFixedInteger(2, 1));
+    fields.put(UsNhField.GPC_SEGMENT_INCLUDED, new EncodableBoolean(true));
+    fields.put(UsNhField.GPC, new EncodableBoolean(false));
+    return fields;
+  }
+
+  @Override
+  protected String encodeSegment(EncodableBitStringFields fields) {
+    String bitString = bitStringEncoder.encode(fields, getFieldNames());
+    String encodedString = base64UrlEncoder.encode(bitString);
+    return encodedString;
+  }
+
+  @Override
+  protected void decodeSegment(String encodedString, EncodableBitStringFields fields) {
+    if(encodedString == null || encodedString.isEmpty()) {
+      this.fields.reset(fields);
+    }
+    try {
+      String bitString = base64UrlEncoder.decode(encodedString);
+      bitStringEncoder.decode(bitString, getFieldNames(), fields);
+    } catch (Exception e) {
+      throw new DecodingException("Unable to decode UsNhGpcSegment '" + encodedString + "'", e);
+    }
+  }
+}
--- iabgpp-encoder/src/test/java/com/iab/gpp/encoder/section/UsNatTest.java
+++ iabgpp-encoder/src/test/java/com/iab/gpp/encoder/section/UsNatTest.java
@@ -14,7 +14,7 @@ public class UsNatTest {
   public void testEncode1() {
 
     UsNat usNat = new UsNat();
-    Assertions.assertEquals("BAAAAAAAAQA.QA", usNat.encode());
+    Assertions.assertEquals("BAAAAAAAAABA.QA", usNat.encode());
   }
 
   @Test
@@ -30,15 +30,15 @@ public class UsNatTest {
     usNat.setFieldValue(UsNatField.SALE_OPT_OUT, 1);
     usNat.setFieldValue(UsNatField.SHARING_OPT_OUT, 1);
     usNat.setFieldValue(UsNatField.TARGETED_ADVERTISING_OPT_OUT, 1);
-    usNat.setFieldValue(UsNatField.SENSITIVE_DATA_PROCESSING, Arrays.asList(2, 1, 0, 2, 1, 0, 2, 1, 0, 2, 1, 0));
-    usNat.setFieldValue(UsNatField.KNOWN_CHILD_SENSITIVE_DATA_CONSENTS, Arrays.asList(2, 1));
+    usNat.setFieldValue(UsNatField.SENSITIVE_DATA_PROCESSING, Arrays.asList(2, 1, 0, 2, 1, 0, 2, 1, 0, 2, 1, 0, 2, 1, 0, 2));
+    usNat.setFieldValue(UsNatField.KNOWN_CHILD_SENSITIVE_DATA_CONSENTS, Arrays.asList(2, 1, 0));
     usNat.setFieldValue(UsNatField.PERSONAL_DATA_CONSENTS, 1);
     usNat.setFieldValue(UsNatField.MSPA_COVERED_TRANSACTION, 1);
     usNat.setFieldValue(UsNatField.MSPA_OPT_OUT_OPTION_MODE, 1);
     usNat.setFieldValue(UsNatField.MSPA_SERVICE_PROVIDER_MODE, 2);
     usNat.setFieldValue(UsNatField.GPC, true);
 
-    Assertions.assertEquals("BVVVkkkklWA.YA", usNat.encode());
+    Assertions.assertEquals("BVVVkkkkkpFY.YA", usNat.encode());
   }
   
   @Test
@@ -575,15 +575,15 @@ public class UsNatTest {
     usNat.setFieldValue(UsNatField.SALE_OPT_OUT, 1);
     usNat.setFieldValue(UsNatField.SHARING_OPT_OUT, 1);
     usNat.setFieldValue(UsNatField.TARGETED_ADVERTISING_OPT_OUT, 1);
-    usNat.setFieldValue(UsNatField.SENSITIVE_DATA_PROCESSING, Arrays.asList(2, 1, 0, 2, 1, 0, 2, 1, 0, 2, 1, 0));
-    usNat.setFieldValue(UsNatField.KNOWN_CHILD_SENSITIVE_DATA_CONSENTS, Arrays.asList(2, 1));
+    usNat.setFieldValue(UsNatField.SENSITIVE_DATA_PROCESSING, Arrays.asList(2, 1, 0, 2, 1, 0, 2, 1, 0, 2, 1, 0, 2, 1, 0, 2));
+    usNat.setFieldValue(UsNatField.KNOWN_CHILD_SENSITIVE_DATA_CONSENTS, Arrays.asList(2, 1, 0));
     usNat.setFieldValue(UsNatField.PERSONAL_DATA_CONSENTS, 1);
     usNat.setFieldValue(UsNatField.MSPA_COVERED_TRANSACTION, 1);
     usNat.setFieldValue(UsNatField.MSPA_OPT_OUT_OPTION_MODE, 1);
     usNat.setFieldValue(UsNatField.MSPA_SERVICE_PROVIDER_MODE, 2);
     usNat.setFieldValue(UsNatField.GPC, true);
 
-    Assertions.assertEquals("BVVVkkkklWA.YA", usNat.encode());
+    Assertions.assertEquals("BVVVkkkkkpFY.YA", usNat.encode());
   }
 
   @Test
@@ -591,12 +591,12 @@ public class UsNatTest {
 
     UsNat usNat = new UsNat();
     usNat.setFieldValue(UsNatField.GPC_SEGMENT_INCLUDED, false);
-    Assertions.assertEquals("BAAAAAAAAQA", usNat.encode());
+    Assertions.assertEquals("BAAAAAAAAABA", usNat.encode());
   }
 
   @Test
   public void testDecode1() throws DecodingException {
-    UsNat usNat = new UsNat("BVVVkkkklWA.YA");
+    UsNat usNat = new UsNat("BVVVkkkkkpFY.YA");
 
     Assertions.assertEquals(1, usNat.getSharingNotice());
     Assertions.assertEquals(1, usNat.getSaleOptOutNotice());
@@ -607,8 +607,8 @@ public class UsNatTest {
     Assertions.assertEquals(1, usNat.getSaleOptOut());
     Assertions.assertEquals(1, usNat.getSharingOptOut());
     Assertions.assertEquals(1, usNat.getTargetedAdvertisingOptOut());
-    Assertions.assertEquals(Arrays.asList(2, 1, 0, 2, 1, 0, 2, 1, 0, 2, 1, 0), usNat.getSensitiveDataProcessing());
-    Assertions.assertEquals(Arrays.asList(2, 1), usNat.getKnownChildSensitiveDataConsents());
+    Assertions.assertEquals(Arrays.asList(2, 1, 0, 2, 1, 0, 2, 1, 0, 2, 1, 0, 2, 1, 0, 2), usNat.getSensitiveDataProcessing());
+    Assertions.assertEquals(Arrays.asList(2, 1, 0), usNat.getKnownChildSensitiveDataConsents());
     Assertions.assertEquals(1, usNat.getPersonalDataConsents());
     Assertions.assertEquals(1, usNat.getMspaCoveredTransaction());
     Assertions.assertEquals(1, usNat.getMspaOptOutOptionMode());
@@ -618,7 +618,7 @@ public class UsNatTest {
 
   @Test
   public void testDecodeWithGpcSegmentExcluded() throws DecodingException {
-    UsNat usNat = new UsNat("BVVVkkkklWA");
+    UsNat usNat = new UsNat("BVVVkkkkkpFY");
 
     Assertions.assertEquals(1, usNat.getSharingNotice());
     Assertions.assertEquals(1, usNat.getSaleOptOutNotice());
@@ -629,8 +629,8 @@ public class UsNatTest {
     Assertions.assertEquals(1, usNat.getSaleOptOut());
     Assertions.assertEquals(1, usNat.getSharingOptOut());
     Assertions.assertEquals(1, usNat.getTargetedAdvertisingOptOut());
-    Assertions.assertEquals(Arrays.asList(2, 1, 0, 2, 1, 0, 2, 1, 0, 2, 1, 0), usNat.getSensitiveDataProcessing());
-    Assertions.assertEquals(Arrays.asList(2, 1), usNat.getKnownChildSensitiveDataConsents());
+    Assertions.assertEquals(Arrays.asList(2, 1, 0, 2, 1, 0, 2, 1, 0, 2, 1, 0, 2, 1, 0, 2), usNat.getSensitiveDataProcessing());
+    Assertions.assertEquals(Arrays.asList(2, 1, 0), usNat.getKnownChildSensitiveDataConsents());
     Assertions.assertEquals(1, usNat.getPersonalDataConsents());
     Assertions.assertEquals(1, usNat.getMspaCoveredTransaction());
     Assertions.assertEquals(1, usNat.getMspaOptOutOptionMode());
