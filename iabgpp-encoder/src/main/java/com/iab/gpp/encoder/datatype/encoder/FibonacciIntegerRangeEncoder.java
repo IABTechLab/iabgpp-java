c3e8b5f0cb97c9b783065c35de2ccfbd8b242b98
James Yuzawa <jtyuzawa@gmail.com>
2025-04-01 13:44:57 -0400

    lower max_size to be 4x current vendor list size

--- iabgpp-encoder/src/main/java/com/iab/gpp/encoder/datatype/encoder/FibonacciIntegerRangeEncoder.java
+++ iabgpp-encoder/src/main/java/com/iab/gpp/encoder/datatype/encoder/FibonacciIntegerRangeEncoder.java
@@ -8,7 +8,7 @@ import com.iab.gpp.encoder.error.DecodingException;
 
 public class FibonacciIntegerRangeEncoder {
 
-  static final int MAX_SIZE = 16384;
+  static final int MAX_SIZE = 4096;
   private static Pattern BITSTRING_VERIFICATION_PATTERN = Pattern.compile("^[0-1]*$", Pattern.CASE_INSENSITIVE);
 
   public static String encode(List<Integer> value) {
